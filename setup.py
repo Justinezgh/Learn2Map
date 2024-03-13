edf3492a077236ea07a91abab02836f4a9ef6ebb
Justine <zghjustinephd@gmail.com>
2024-03-13 23:40:35 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='learn2map',
+    version='1.0',
+    description=' Learning complex transformations through VAE',
+    author='Justinezgh',
+    packages=['learn2map'],
+    install_requires=[
+        'jax-cosmo',
+        'numpyro',
+        'lenstools',
+        'sbi_lens @ git+https://github.com/DifferentiableUniverseInitiative/sbi_lens.git'
+    ]
+)
